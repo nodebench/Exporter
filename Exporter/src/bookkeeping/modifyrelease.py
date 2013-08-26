2f93f1d1b58178837916980b040a6d10a78f25d9
nodebench <nodebench@gmail.com>
2013-08-26 22:46:47 +0530

    release version generators added.

---
+++ Exporter/src/bookkeeping/modifyrelease.py
@@ -0,0 +1,59 @@
+# -*- coding: utf8 -*-
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+# --------------------------------------------------------------------------
+# Blender Version                     2.68
+# Exporter Version                    0.0.1
+# Created on                          26-Aug-2013
+# Author                              NodeBench
+# --------------------------------------------------------------------------
+
+import os
+
+def version_bump():
+    '''
+    Run this module after creating a release branch, to change the version number
+    of the exporter in all files.
+    Then manually merge the release branch to the master branch to finalise the release. 
+    '''
+    zip_version = '0.0.3'
+    search_path = '../sunflow'
+    bump(zip_version, search_path)
+
+def bump(next_version , search_path):
+    find_string = '# Exporter Version'    
+    search_full = os.path.abspath(search_path)    
+    for base, dummy_dirs, files in os.walk(search_full):
+        for file_in in files:
+            fn = os.path.join(base, file_in)
+            if not os.path.exists(fn): continue
+            cache = []
+            curfile = open(fn, 'r')
+            for lines in curfile:
+                if lines.startswith(find_string):
+                    lines = '# Exporter Version                    ' + next_version + '\n'
+                cache.append(lines)
+            curfile.close()
+            curfile = open(fn, 'w')
+            for lines in cache:
+                curfile.write("%s" % lines)
+            curfile.close()            
+            
+if __name__ == '__main__':
+    version_bump()
---
+++ Exporter/src/bookkeeping/zipit.py
@@ -0,0 +1,68 @@
+# -*- coding: utf8 -*-
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+# --------------------------------------------------------------------------
+# Blender Version                     2.68
+# Exporter Version                    0.0.1
+# Created on                          25-Aug-2013
+# Author                              NodeBench
+# --------------------------------------------------------------------------
+
+
+import os
+import zipfile
+
+
+def create_release_package():
+    '''
+    create a taged release version from github will not do the trick.
+    Since the folder structure in eclipse git is like proj/proj/src/sunflow
+    and when tagged and zipped will contain all these sub folders
+    but inside blender addon folder it requires in a form like 
+    sunflow/modules. so zipped installer should only contain this sunflow sub folder.
+    '''
+    zip_version = '0.0.3'
+    search_path = '../sunflow'
+    zipit(zip_version, search_path)
+
+def zipit(zip_version, rel_to_path):
+    zip_to_path = r'E:\DevelProjects\gitRepository\Exporter.wiki\releases'
+    
+    rel_to_path = os.path.abspath(rel_to_path)
+    zip_to_path = os.path.abspath(zip_to_path)
+    
+    zip_name = os.path.basename(rel_to_path)
+    pth_in_zip = os.path.dirname(rel_to_path)
+    zip_file = zip_name + '_v' + zip_version + '.zip'
+    
+    print(zip_file)
+    zip_path = os.path.join(zip_to_path, zip_file)
+    
+    if os.path.exists(zip_path):
+        os.unlink(zip_path)
+    
+    zip_f = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+    rootlen = len(pth_in_zip) + 1
+    for base, dummy_dirs, files in os.walk(rel_to_path):
+        for file_in in files:
+            fn = os.path.join(base, file_in)
+            zip_f.write(fn, fn[rootlen:])
+
+if __name__ == '__main__':
+    create_release_package()
